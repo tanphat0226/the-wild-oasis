ac9ece1e52d36fbf988ba85a26c48ef378fa4ca8
ryanpham <ryanphamit@gmail.com>
2025-09-04 21:07:00 +0700

    Update navigation structure with MainNav and SideBar components; replace favicon and add react-icons dependency

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import Logo from './Logo'
+import MainNav from './MainNav'
 
 const StyledSideBar = styled.aside`
 	background-color: var(--color-grey-0);
@@ -6,10 +8,18 @@ const StyledSideBar = styled.aside`
 	border-right: 1px solid var(--color-grey-100);
 
 	grid-row: 1 / -1;
+	display: flex;
+	flex-direction: column;
+	gap: 3.2rem;
 `
 
 const SideBar = () => {
-	return <StyledSideBar>SideBar</StyledSideBar>
+	return (
+		<StyledSideBar>
+			<Logo />
+			<MainNav />
+		</StyledSideBar>
+	)
 }
 
 export default SideBar
