cdfb733e9df3ad917a39b613afbfd58cd3570585
ryanpham <ryanphamit@gmail.com>
2025-09-08 20:52:54 +0700

    Refactor CabinRow and CabinTable components to utilize new Table component; enhance table structure and styling for improved readability and maintainability

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -8,18 +8,7 @@ import CabinForm from './CabinForm'
 import { useCreateCabin } from './useCreateCabin'
 import { useDeleteCabin } from './useDeleteCabin'
 import ConfirmDelete from '../../ui/ConfirmDelete'
-
-const TableRow = styled.div`
-	display: grid;
-	grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-	column-gap: 2.4rem;
-	align-items: center;
-	padding: 1.4rem 2.4rem;
-
-	&:not(:last-child) {
-		border-bottom: 1px solid var(--color-grey-100);
-	}
-`
+import Table from '../../ui/Table'
 
 const Img = styled.img`
 	display: block;
@@ -67,7 +56,7 @@ const CabinRow = ({ cabin }) => {
 	}
 
 	return (
-		<TableRow role='row'>
+		<Table.Row>
 			<Img src={image} />
 			<Cabin>{name}</Cabin>
 			<div>Fits up to {maxCapacity} guests</div>
@@ -106,7 +95,7 @@ const CabinRow = ({ cabin }) => {
 					</Modal.Window>
 				</Modal>
 			</div>
-		</TableRow>
+		</Table.Row>
 	)
 }
 
