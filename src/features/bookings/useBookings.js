ecb0328bb51743a96c5b252d157ab410adefbf28
ryanpham <ryanphamit@gmail.com>
2025-09-10 20:12:13 +0700

    Implement sorting functionality in useBookings and apiBookings: update getBookings to handle sorting parameters and adjust queryKey in useBookings.

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { getBookings } from '../../services/apiBookings'
 import { useSearchParams } from 'react-router-dom'
-import { BOOKINGS_STATUS } from '../../utils/constant'
+import { BOOKINGS_SORT_OPTIONS, BOOKINGS_STATUS } from '../../utils/constant'
 
 export const useBookings = () => {
 	const [searchParams] = useSearchParams()
@@ -13,14 +13,21 @@ export const useBookings = () => {
 			? null
 			: { field: 'status', value: filterValue }
 
+	// SORT
+	const sortByRaw =
+		searchParams.get('sortBy') || BOOKINGS_SORT_OPTIONS.START_DATE_DESC
+
+	const [fied, direction] = sortByRaw.split('-')
+	const sortBy = { field: fied, direction }
+
 	//  FETCH BOOKINGS
 	const {
 		isPending: isLoading,
 		data: bookings,
 		error,
 	} = useQuery({
-		queryKey: ['bookings', filter], // Unique key for the query
-		queryFn: () => getBookings({ filter }), // Function that fetches the data
+		queryKey: ['bookings', filter, sortBy], // Unique key for the query
+		queryFn: () => getBookings({ filter, sortBy }), // Function that fetches the data
 	})
 
 	return { isLoading, bookings, error }
