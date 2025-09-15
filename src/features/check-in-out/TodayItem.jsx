a29f847c37142c267f8b1e18861499c59dc8ac46
ryanpham <ryanphamit@gmail.com>
2025-09-15 20:08:24 +0700

    feat: implement TodayActivity component and integrate into DashboardLayout; enhance CheckoutButton and TodayItem for improved activity display

--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,20 +1,63 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { BOOKINGS_STATUS } from '../../utils/constants'
+import Tag from '../../ui/Tag'
+import { Flag } from '../../ui/Flag'
+import Button from '../../ui/Button'
+import { Link } from 'react-router-dom'
+import CheckoutButton from './CheckoutButton'
 
 const StyledTodayItem = styled.li`
-  display: grid;
-  grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
-  gap: 1.2rem;
-  align-items: center;
+	display: grid;
+	grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
+	gap: 1.2rem;
+	align-items: center;
 
-  font-size: 1.4rem;
-  padding: 0.8rem 0;
-  border-bottom: 1px solid var(--color-grey-100);
+	font-size: 1.4rem;
+	padding: 0.8rem 0;
+	border-bottom: 1px solid var(--color-grey-100);
 
-  &:first-child {
-    border-top: 1px solid var(--color-grey-100);
-  }
-`;
+	&:first-child {
+		border-top: 1px solid var(--color-grey-100);
+	}
+`
 
 const Guest = styled.div`
-  font-weight: 500;
-`;
+	font-weight: 500;
+`
+
+function TodayItem({ activity }) {
+	const { id, status, guests, numNights } = activity
+	return (
+		<StyledTodayItem>
+			{status === BOOKINGS_STATUS.UNCONFIRMED && (
+				<Tag type='green'>Arriving</Tag>
+			)}
+
+			{status === BOOKINGS_STATUS.CHECKED_IN && (
+				<Tag type='blue'>Departing</Tag>
+			)}
+
+			<Flag src={guests.countryFlag} alt={`Flag of ${guests.country}`} />
+			<Guest>{guests.fullName}</Guest>
+
+			<div>{numNights} nights</div>
+
+			{status === BOOKINGS_STATUS.UNCONFIRMED && (
+				<Button
+					size='small'
+					variant='primary'
+					as={Link}
+					to={`/checkin/${id}`}
+				>
+					Check-in
+				</Button>
+			)}
+
+			{status === BOOKINGS_STATUS.CHECKED_IN && (
+				<CheckoutButton bookingId={id} />
+			)}
+		</StyledTodayItem>
+	)
+}
+
+export default TodayItem
--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,6 +6,7 @@ import SalesChart from './SalesChart'
 import Stats from './Stats'
 import { useRecentBookings } from './useRecentBookings'
 import { useRecentStays } from './useRecentStays'
+import TodayActivity from '../check-in-out/TodayActivity'
 
 const StyledDashboardLayout = styled.div`
 	display: grid;
@@ -33,8 +34,7 @@ function DashboardLayout() {
 				numDays={numDays}
 				cabinCount={cabins.length}
 			/>
-			<div>Today's Activity</div>
-
+			<TodayActivity />
 			<DurationChart confirmedStays={confirmedStays} />
 			<SalesChart bookings={bookings} numDays={numDays} />
 		</StyledDashboardLayout>
